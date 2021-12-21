d32a63543fb44a389eebce37936970ec6124d87e
Taeuk Gwak <gwak2837@kakao.com>
2021-12-21 19:34:28 +0900

    First commit

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  i18n: {
+    locales: ['ko', 'en'],
+    defaultLocale: 'ko',
+  },
+  images: {
+    domains: ['storage.googleapis.com'],
+  },
+  poweredByHeader: process.env.NODE_ENV === 'development',
+  pwa: {
+    buildExcludes: [/middleware-manifest\.json$/],
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
+  },
+  reactStrictMode: process.env.NODE_ENV === 'development',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+})
