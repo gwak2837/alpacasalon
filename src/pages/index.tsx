47610bcb6e3370adf9caa9c9ac388bbcd65859e7
Taeuk Gwak <gwak2837@kakao.com>
2022-01-03 13:59:07 +0900

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
+import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
+import { useRecoilValue } from 'recoil'
 import Navigation from 'src/layouts/Navigation'
+import { currentUser } from 'src/models/recoil'
+import AlpacasalonText from 'src/svgs/alpacasalon-text.svg'
+import styled from 'styled-components'
+
+const WhiteButton = styled.button`
+  border: 1px solid #eee;
+  border-radius: 5px;
+
+  padding: 0.7rem;
+`
 
 export default function HomePage() {
-  return <div>홈페이지</div>
+  const router = useRouter()
+  const { nickname } = useRecoilValue(currentUser)
+
+  return (
+    <div>
+      <AlpacasalonText />
+      {!nickname && <WhiteButton onClick={() => router.push('/login')}>로그인</WhiteButton>}
+    </div>
+  )
 }
 
 HomePage.getLayout = function getLayout(page: ReactElement) {
