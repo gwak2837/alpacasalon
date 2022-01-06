42e22bd307c2ada845c83dc981a459602982782a
Taeuk Gwak <gwak2837@kakao.com>
2022-01-06 15:53:15 +0900

    Add group cover image

--- src/pages/group/[id]/index.tsx
+++ src/pages/group/[id]/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
 import { toastApolloError } from 'src/apollo/error'
@@ -11,7 +12,32 @@ import {
 } from 'src/graphql/generated/types-and-hooks'
 import useNeedToLogin from 'src/hooks/useNeedToLogin'
 import { PrimaryButton } from 'src/pages/post'
+import BackIcon from 'src/svgs/back-icon.svg'
 import WriteIcon from 'src/svgs/write-icon.svg'
+import styled from 'styled-components'
+
+const Frame16to7 = styled.div`
+  aspect-ratio: 16 / 7;
+  position: relative;
+`
+
+const Absolute = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 3rem;
+  z-index: 1;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > svg {
+    cursor: pointer;
+    width: 3rem;
+    padding: 0.5rem;
+  }
+`
 
 const description = ''
 
@@ -51,12 +77,26 @@ export default function GroupDetailPage() {
     }
   }
 
+  function goBack() {
+    router.back()
+  }
+
   useNeedToLogin()
 
   return (
     <PageHead title=" - 알파카살롱" description={description}>
+      <Frame16to7>
+        <Absolute>
+          <BackIcon onClick={goBack} />
+        </Absolute>
+        <Image
+          src={group?.imageUrl ?? '/images/default-image.webp'}
+          alt="group cover"
+          layout="fill"
+          objectFit="cover"
+        />
+      </Frame16to7>
       <pre style={{ overflow: 'scroll' }}>{JSON.stringify(group, null, 2)}</pre>
-
       <h3>게시글</h3>
       {posts?.map((post) => (
         <PostCard key={post.id} post={post as Post} />
@@ -66,8 +106,7 @@ export default function GroupDetailPage() {
         {group?.isJoined ? '탈퇴하기' : '+ 이 그룹 가입하기'}
         {loading && '...'}
       </button>
-
-      <PrimaryButton disabled={!groupId} onClick={goToPostCreationPage}>
+      <PrimaryButton disabled={!groupId || !group?.isJoined} onClick={goToPostCreationPage}>
         <WriteIcon />
         글쓰기
       </PrimaryButton>
